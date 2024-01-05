972d13dbe91e15f342d68eee126bfe04c6079b14
SetiadyAnwar <setiadyanwar@gmail.com>
2024-01-05 18:44:53 +0700

    contact1.0

--- dist/js/script.js
+++ dist/js/script.js
@@ -111,7 +111,7 @@ uiuxTab.classList.add('active');
 
 //Typejs
 var typed = new Typed('#autotext',{
-    strings : ["FrontEnd Web Developer","Frontend Mobile Developer","Graphic Designer", "Videographer","Fotographer"],
+    strings : ["FrontEnd Web ","Frontend MobileApp","Graphic Designer", "Videographer","Fotographer"],
     typeSpeed :100,
     backSpeed :100,
     loop :true,
@@ -139,6 +139,36 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
+
+//   Contact
+// Ambil elemen tombol dan modal menggunakan selektor
+const modalToggleBtn = document.querySelector('[data-modal-toggle="crud-modal"]');
+const modal = document.getElementById('crud-modal');
+const modalOverlay = document.getElementById('modal-overlay');
+const modalCloseBtn = document.querySelector('[data-modal-close="crud-modal"]');
+
+// Fungsi untuk menampilkan modal
+function showModal() {
+  modal.classList.remove('hidden');
+  modalOverlay.classList.remove('hidden');
+  document.body.classList.add('overflow-hidden');
+}
+
+// Fungsi untuk menyembunyikan modal
+function hideModal() {
+  modal.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
+  document.body.classList.remove('overflow-hidden');
+}
+
+// Tambahkan event listener untuk mengatur tampilan modal
+modalToggleBtn.addEventListener('click', showModal);
+
+// Tambahkan event listener untuk menyembunyikan modal saat overlay diklik
+modalOverlay.addEventListener('click', hideModal);
+
+// Tambahkan event listener untuk menyembunyikan modal saat tombol close diklik
+modalCloseBtn.addEventListener('click', hideModal);
 
 
 //detailportof
